f7de766a028347dee0d66edeef9a15adc25fd686
krzysztofee <gitaragra@gmail.com>
2024-02-05 12:28:11 +0100

    Add audio input and handleFileSelect function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+
+const handleFileSelect = () => {
+  const fileInput = document.getElementById("audioFile");
+  const audioPreview = document.getElementById("audioPreview");
+
+  const selectedFile = fileInput.files[0];
+
+  if (selectedFile) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      audioPreview.src = e.target.result;
+    };
+
+    reader.readAsDataURL(selectedFile);
+  } else {
+    audioPreview.src = "";
+  }
+};
